d9e4d58de91935c7c4f3f88d5dddd9d5ee7e6cd2
Yanik dos Santos <68445853+yrsvntos@users.noreply.github.com>
2023-11-30 19:13:36 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Olá Mundo, apenas agradecer pelo suporte incodicional que a minha namorada e futura esposa Yara Ribeiro me dá incasavelmente, eu só tenho a dizer a ela o quão eu lhe amo muito.</h1>
+    return <h1>Olá Mundo, apenas agradecer pelo suporte incodicional que a minha namorada e futura esposa Yara Ribeiro me dá incasavelmente, eu só tenho a dizer a ela o quão eu lhe amo muito. ❤️</h1>
 }
 
 export default Home;
