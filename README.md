b57d066364d07bdef3d1c99a2c111e9be1ce24cb
Yanik dos Santos <68445853+yrsvntos@users.noreply.github.com>
2023-12-11 19:43:29 +0000

    adiciona scripts `lint:check e lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo a programar
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home(){
-    return <h1>Olá Mundo, apenas agradecer pelo suporte incodicional que a minha namorada e futura esposa Yara Ribeiro me dá incasavelmente, eu só tenho a dizer a ela o quão eu lhe amo muito. ❤️</h1>
+function Home() {
+  return (
+    <h1>
+      Olá Mundo, apenas agradecer pelo suporte incodicional que a minha namorada
+      e futura esposa Yara Ribeiro me dá incasavelmente, eu só tenho a dizer a
+      ela o quão eu lhe amo muito. ❤️
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("Olá deu certo");
+}
+
+function teste2() {
+  console.log("Testando");
 }
 
 export default Home;
